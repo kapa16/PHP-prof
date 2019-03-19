b5a900132a6994ee7b5795a220a44ca85f6596df
kapa <kapa16@inbox.ru>
2019-03-20 00:14:40 +0500

    gallery controllers

--- Lesson3/public/image_view.php
+++ Lesson3/public/image_view.php
@@ -1,7 +1,24 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kapa
- * Date: 20.03.2019
- * Time: 0:08
- */
+
+use App\Engine\Templater;
+
+require_once '../vendor/autoload.php';
+
+//use \App\Views\View;
+//
+//$view = new View();
+//$view->title = 'Gallery';
+//$view->header = 'Gallery';
+
+//echo $view->render('layout.twig');
+$gallery = \App\Models\Gallery::ge();
+
+$data = [
+    'title' => 'Gallery',
+    'header' => 'Gallery',
+    'images' => $gallery
+];
+
+$twig = Templater::getInstance()->twig;
+$indexTemplate = $twig->load('image_view.twig');
+echo $indexTemplate->render($data);
--- Lesson3/public/index.php
+++ Lesson3/public/index.php
@@ -1,12 +1,24 @@
 <?php
 
+use App\Engine\Templater;
+
 require_once '../vendor/autoload.php';
 
-use \App\Engine\Templater;
+//use \App\Views\View;
+//
+//$view = new View();
+//$view->title = 'Gallery';
+//$view->header = 'Gallery';
+
+//echo $view->render('layout.twig');
+$gallery = \App\Models\Gallery::getAll();
+
+$data = [
+    'title' => 'Gallery',
+    'header' => 'Gallery',
+    'images' => $gallery
+];
 
 $twig = Templater::getInstance()->twig;
-$indexTemplate = $twig->load('layout.twig');
-echo $indexTemplate->render([
-    'title' => 'Gallery',
-    'header' => 'Gallery'
-]);
+$indexTemplate = $twig->load('gallery.twig');
+echo $indexTemplate->render($data);
