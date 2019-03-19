5881981736c973c0471b90f0d510ed0a388b13e0
kapa16 <kapa16@inbox.ru>
2019-03-19 21:51:55 +0500

    view

--- Lesson3/App/Views/View.php
+++ Lesson3/App/Views/View.php
@@ -4,5 +4,20 @@ namespace App\Views;
 
 class View
 {
+    private $data = [];
 
+    public function __get($name)
+    {
+        // TODO: Implement __get() method.
+    }
+
+    public function __set($name, $value)
+    {
+        // TODO: Implement __set() method.
+    }
+
+    public function __isset($name)
+    {
+        // TODO: Implement __isset() method.
+    }
 }
