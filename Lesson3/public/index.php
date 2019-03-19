042b3822945cd6c9187fa49eef3cd3a1ace19c91
kapa16 <kapa16@inbox.ru>
2019-03-19 21:52:11 +0500

    render template

--- Lesson3/public/index.php
+++ Lesson3/public/index.php
@@ -1,12 +1,12 @@
 <?php
 
+require_once '../vendor/autoload.php';
+
 use \App\Engine\Templater;
 
-require_once '../config/config.php';
-
-spl_autoload_register(function ($class) {
-    $class = str_replace('\\', '/', $class);
-    require_once __DIR__ . '/../' . $class . '.php';
-});
-
-$twig = Templater::getInstance();
+$twig = Templater::getInstance()->twig;
+$indexTemplate = $twig->load('layout.twig');
+echo $indexTemplate->render([
+    'title' => 'Gallery',
+    'header' => 'Gallery'
+]);
