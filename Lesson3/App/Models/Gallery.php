d7d24091f4bd44e7b24383638e2921b206f372d0
kapa <kapa16@inbox.ru>
2019-03-20 00:13:21 +0500

    gallery model

---
+++ Lesson3/App/Models/Gallery.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kapa
+ * Date: 19.03.2019
+ * Time: 22:57
+ */
+
+namespace App\Models;
+
+
+class Gallery
+{
+
+}
---
+++ Lesson3/public/image_view.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kapa
+ * Date: 20.03.2019
+ * Time: 0:08
+ */
