1c38e1f47cf50bacce126c9fa7956e9aae12c511
Pavel <kapa16@inbox.ru>
2019-03-21 12:09:13 +0500

    controller for gallery & image view

--- Lesson3/App/Controllers/GalleryImageController.php
+++ Lesson3/App/Controllers/GalleryImageController.php
@@ -2,7 +2,31 @@
 
 namespace App\Controllers;
 
-class GalleryImage
-{
+use App\Models\Image;
 
+class GalleryImageController extends Controller
+{
+    public function actionGallery()
+    {
+        $sortFields = ['views'];
+        $gallery = Image::getAll($sortFields);
+        $data = [
+            'title' => 'Gallery',
+            'header' => 'Gallery',
+            'images' => $gallery
+        ];
+        return $this->getView($data);
+    }
+
+    public function actionSingleImage($id)
+    {
+        $image = Image::getOne($id);
+        $image->increaseImageViews()->update();
+        $data = [
+            'title' => 'Gallery',
+            'header' => 'Full-size image',
+            'image' => $image
+        ];
+        return $this->getView($data);
+    }
 }
