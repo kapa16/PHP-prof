3a823f99049a8094a628d18e063d23e3b56c0a1f
Pavel <kapa16@inbox.ru>
2019-03-21 12:09:07 +0500

    abstract controller class

--- Lesson3/App/Controllers/Controller.php
+++ Lesson3/App/Controllers/Controller.php
@@ -2,7 +2,32 @@
 
 namespace App\Controllers;
 
-class Controller
+use App\Engine\Templater;
+
+abstract class Controller
 {
+    private $templateName;
+
+    /**
+     * Controller constructor.
+     * @param string $templateName
+     */
+    public function __construct(string $templateName)
+    {
+        $this->templateName = $templateName;
+    }
+
+    /**
+     * Return view from template
+     * @param $data array parameters for template
+     * @return mixed
+     */
+    protected function getView($data)
+    {
+        $twig = Templater::getInstance()->twig;
+        $indexTemplate = $twig->load($this->templateName);
+        return $indexTemplate->render($data);
+    }
+
 
 }
