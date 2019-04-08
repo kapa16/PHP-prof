262967a1e49a80c5f1431091ad7629d79f06b560
Pavel <kapa16@inbox.ru>
2019-04-08 16:47:37 +0500

    work with sessions class

--- Lesson7/App/Engine/Session.php
+++ Lesson7/App/Engine/Session.php
@@ -1,10 +1,31 @@
 <?php
 
-
 namespace App\Engine;
 
+use App\Models\User;
 
+/**
+ * @property array|string visitedPages
+ * @property User|string user
+ */
 class Session
 {
+    public function __construct()
+    {
+        session_start();
+    }
+    public function __get($name)
+    {
+        return $_SESSION[$name] ?? '';
+    }
 
+    public function __set($name, $value)
+    {
+        $_SESSION[$name] = $value;
+    }
+
+    public function __isset($name)
+    {
+        return isset($_SESSION[$name]);
+    }
 }
